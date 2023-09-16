a8b15a9feac602035d3940402c36a8702dab7f4d
Gul-e-Nayab <111217531+gulenayab@users.noreply.github.com>
2023-09-16 18:03:17 +0500

    Add files via upload

---
+++ src/components/CSS_Styling_React/Inline.js
@@ -0,0 +1,16 @@
+import React from 'react'
+ 
+const heading = 
+{
+    fontSize : '50px',
+    color:'orange'
+}
+function Inline() {
+  return (
+    <div>
+      <h1 style = {heading}>Gul-e-Nayab</h1>
+    </div>
+  )
+}
+
+export default Inline
